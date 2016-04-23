aee99e53335855937135ddd4dec8dadf335d7222
Rostislav Yavorskiy <ryavorsky@gmail.com>
2016-04-23 11:58:05 +0200

    Update xmlparse.py

--- xmlparse.py
+++ xmlparse.py
@@ -1,9 +1,20 @@
 import xml.etree.ElementTree as etree
-for i in range(5):
+for i in range(11):
     tree = etree.parse('d://Keywords/news{}.xml'.format(i))
-    print('d://Keywords/news{}.xml'.format(i))
+    print('\nd://Keywords/news{}.xml'.format(i))
     root = tree.getroot()
 
-    for e in root.findall('.//w/ana[1]'):
-         print(e.attrib['lex'])
+    res = dict()
+    for e in root.findall('.//ana'):
+        word = e.attrib['lex']
+        if word in res:
+            res[word] += 1
+        else:
+            res[word] =1
+    words = list(res.keys())
+    for w in words:
+        if res[w] <= 2:
+            del res[w]
+    res1 = [[w,res[w]] for w in res]
+    print(res)
          
